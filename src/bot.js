f35b9f9a6dfbf13a63fe8687166ac77ef2c904e9
Curry <53649280+CurryBoi29@users.noreply.github.com>
2021-10-22 22:46:42 +0100

    oops

--- src/bot.js
+++ src/bot.js
@@ -13,4 +13,4 @@ const loadEvents = require("./load-events");
 loadCommands.load(Client, "./commands");
 loadEvents.load(Client, "./events");
 
-Client.login("ODk5NDE1OTMwMjY0NzcyNzU4.YWycXA.9QY9d15A_E-LSwB75venEMj5new");
+Client.login("");
