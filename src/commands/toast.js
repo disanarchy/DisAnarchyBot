28b73e0aa3b60954efce9eab22a4a6f78a327f5a
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-10-25 20:09:51 -0400

    prevent self-toasting

--- src/commands/toast.js
+++ src/commands/toast.js
@@ -1,7 +1,11 @@
 module.exports = {
     name: "toast",
     execute: async (client, message, args) => {
-        let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+        let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+
+        if (!user) {
+            return message.reply("You must provide a user or user id to toast.");
+        }
 
         message.reply(`${message.author} just toasted ${user}!!! Get toasted!`);
     }
