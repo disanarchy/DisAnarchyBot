1832974f55dd43f3d5f388b55e91d6a65117ad24
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-10-25 20:06:32 -0400

    placed the arguments incorrectly

--- src/commands/toast.js
+++ src/commands/toast.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "toast",
-    execute: async (message, client, args) => {
-        let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+    execute: async (client, message, args) => {
+        let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
 
         message.reply(`${message.author} just toasted ${user}!!! Get toasted!`);
     }
