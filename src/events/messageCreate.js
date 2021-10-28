c565597e763b2a0ae98265b523dbf0eb0b7c66fa
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-10-28 16:35:06 -0400

    woops

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,17 +1,19 @@
 const prefix = "?"
+const { client } = require("../index.js");
 
-module.exports = {
-    name: "messageCreate",
-    execute: async (client, message) => {
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-        if (message.channel.id == "903376643215732786") return; // blocks commands from the #chat-bot channel
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (message.channel.id != "903376643215732786") {
 
-        const args = message.content.slice(prefix.length).trim().split(" ");
-        const command = args.shift().toLowerCase();
+    } else {
+        return;
+    } // blocks commands from the #chat-bot channel
 
-        if (!client.commands.get(command)) return;
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
 
-        client.commands.get(command).execute(client, message, args);
-    }
-}
+    if (!client.commands.get(command)) return;
+
+    client.commands.get(command).execute(client, message, args);
+});
