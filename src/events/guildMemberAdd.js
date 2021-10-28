a09d5f438f6b0bf3286b47907a2c294a70f55bcb
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-10-28 12:41:18 -0400

    fix verification

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,4 +1,6 @@
 const { client } = require("../index.js");
+const db = require("quick.db");
+const verifications = new db.table("verifications");
 
 client.on("guildMemberAdd", (member) => {
     let channelId = "903085764412325938";
@@ -10,5 +12,13 @@ client.on("guildMemberAdd", (member) => {
         let channel = client.guilds.cache.get(guildId).channels.cache.get(channelId);
 
         channel.send(`${member} welcome to DisAnarchy! To gain access to our server please go into <#899426674448420874> and run ?verify!`);
+    }
+
+    if (verifications.has(member.id)) {
+        let roleId = "899426529916899339";
+
+        let role = client.guilds.cache.get(guildId).roles.cache.get(roleId);
+
+        member.roles.add(role);
     }
 });
