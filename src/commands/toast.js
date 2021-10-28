a86c88af3de0dbc0330a1a26c88faa2f3eb3b4ab
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-10-28 12:33:41 -0400

    fix a bug

--- src/commands/toast.js
+++ src/commands/toast.js
@@ -1,7 +1,15 @@
 module.exports = {
     name: "toast",
     execute: async (client, message, args) => {
-        let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+        if (message.author.id == "599204289088585738") { // ban dotto from command
+            return
+        }
+
+        if (message.mentions.roles.size < 0) {
+            return message.reply("Do not mention a role.");
+        }
+
+        let user = message.mentions.users.first() || await message.guild.members.fetch(args[0]);
 
         if (!user) {
             return message.reply("You must provide a user or user id to toast.");
